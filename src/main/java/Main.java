70e0924bf35f19847c6a36e58adf28bf228db3be
Elizaveta I <ishutinaem@gmail.com>
2024-11-17 20:05:47 +0300

    Добавлен функционал по обработке ошибок и выводу информации о содержимом файле

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
@@ -17,9 +19,48 @@ public class Main {
                 count += 1;
                 System.out.println("Файл существует, путь указан верно");
                 System.out.println("Это файл номер " + count);
+
+
+                try {
+                    FileReader fileReader = new FileReader(path);
+                    BufferedReader reader =
+                            new BufferedReader(fileReader);
+                    String line;
+                    int totalLines = 0;
+                    int maxLength = 0;
+                    int minLength = Integer.MAX_VALUE;
+                    while ((line = reader.readLine()) != null) {
+                        int length = line.length();
+                        if (length > 1024) {
+                            throw new LineTooLongException("В файле есть строка длиннее 1024 символов. ");
+                        }
+                        totalLines++;
+                        if (length > maxLength) {
+                            maxLength = length;
+                        }
+                        if (length < minLength) {
+                            minLength = length;
+                        }
+                    }
+
+                    System.out.println("Общее количество строк в файле: " + totalLines);
+                    System.out.println("Длина самой длинной строки в файле: " + maxLength);
+                    System.out.println("Длина самой короткой строки в файле: " + minLength);
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+
             } else {
                 System.out.println("Файл не существует или указан неверный путь");
             }
         }
+    }
+}
+
+class LineTooLongException extends RuntimeException {
+    public LineTooLongException(String message) {
+        super(message);
+
     }
 }
